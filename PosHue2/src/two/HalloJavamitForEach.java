feb1d375161fd43521d901ad7c93e53186888df5
Leonhard <schmidmairl22102@sus.htl-grieskirchen.at>
2024-09-30 20:16:48 +0200

    Add a list of type List<String> in HelloJavaWithForEach class

--- PosHue2/src/two/HalloJavamitForEach.java
+++ PosHue2/src/two/HalloJavamitForEach.java
@@ -1,18 +1,21 @@
 package two;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HalloJavamitForEach {
 
 
     public static void main(String[] args) {
-//       List<String> list = new ArrayList<>();
-//       list.add("Hello");
-//       list.add("World");
-//       list.add("Java");
-//        for (String s : list) {
-//            System.out.println(s);
-//
-//        }
-//        list.forEach(System.out::println);
+       List<String> list = new ArrayList<>();
+       list.add("Hello");
+       list.add("World");
+       list.add("Java");
+        for (String s : list) {
+            System.out.println(s);
+
+        }
+        list.forEach(System.out::println);
 
 
 
